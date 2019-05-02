74a871c762985e7735b78d38b2d8b16b88cafd2f
Acer <mullokandov@gmail.com>
2019-05-02 21:01:58 +0200

    moi novye izmenenija

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello from Andrew");
         System.out.println("Hello again");
+        System.out.println("Hello from Boris")
         while(flag != false) {
             System.out.println("I'm alive!");
             Thread.sleep(1000);
